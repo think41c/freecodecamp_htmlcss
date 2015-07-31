cc9d1b932175be44df2c3e4954828d0dde18c0c6
Tony DiNitto <think41c@gmail.com>
2015-07-31 16:37:36 -0500

    Waypoint 86. Factorial bonfire.

--- bonfire.js
+++ bonfire.js
@@ -1,11 +1,25 @@
-function reverseString(str) {
-  str = str.split("");
-  console.log(str);
-  str = str.reverse();
-  console.log(str);
-  str = str.join('');
-  console.log(str);
-  return str;
+function factorialize(num) {
+ 
+  console.log("The num coming in is " + num);
+  console.log("*********");
+  
+  var count = 1;
+  var hold  = 1;
+  var result = 1;
+  for (var i=1; i<num+1; i+=1) {
+    console.log("i is " + i);
+    result = result * i+1; 
+    new_result = result * i;
+    console.log("i+1 = " + (i+1) + " result = " + result);
+    console.log(new_result);
+  }
+
+  //  1*2*3*4*5
+  //  1*2=2, 2*3=6, 6*4=24, 24*5 = 120
+  //  120 total 
+  
+  return num;
+  
 }
 
-reverseString('hello');
+console.log(factorialize(4));

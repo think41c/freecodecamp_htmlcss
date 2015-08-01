470cffcc5cb3e4ae16b33f1b11a9bb788fcf3699
Tony DiNitto <think41c@gmail.com>
2015-07-31 21:55:50 -0500

    Waypoint 86. Factorialize.

--- bonfire.js
+++ bonfire.js
@@ -1,25 +1,8 @@
-function factorialize(num) {
- 
-  console.log("The num coming in is " + num);
-  console.log("*********");
-  
-  var count = 1;
-  var hold  = 1;
-  var result = 1;
-  for (var i=1; i<num+1; i+=1) {
-    console.log("i is " + i);
-    result = result * i+1; 
-    new_result = result * i;
-    console.log("i+1 = " + (i+1) + " result = " + result);
-    console.log(new_result);
+function xxx(num){
+  var holder = 1;
+  for (var i=2; i<=num; i++){
+    holder *= i;
   }
-
-  //  1*2*3*4*5
-  //  1*2=2, 2*3=6, 6*4=24, 24*5 = 120
-  //  120 total 
-  
-  return num;
-  
+  return holder;
 }
-
-console.log(factorialize(4));
+console.log(xxx(5));

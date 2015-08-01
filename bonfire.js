6297f03d1f0728ab95d81086c29d0b20559045e2
Tony DiNitto <think41c@gmail.com>
2015-08-01 18:39:20 -0500

    Waypoint 89. Largest number - beginning to iterate through both dimensions of array.

--- bonfire.js
+++ bonfire.js
@@ -1,9 +1,10 @@
 function largestOfFour(arr) {
-  for (var i=0; i<arr.length; i++)
-    if (arr[i][i] > arr[i][i+1]){
+  for (var block=0; block<arr.length; block++){
+    for (var i=0; i<arr.length; i++) {
+    if (arr[block][i] > arr[block][i+1]){
       console.log("It's larger " + arr[i][i]);
+      }
     }
+  }
   return arr;
 }
-
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));

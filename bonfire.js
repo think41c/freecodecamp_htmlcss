3fa751fbb4ce5f1936d3358755dd5279d5f14c7e
Tony DiNitto <think41c@gmail.com>
2015-08-01 18:58:25 -0500

    Waypoint 89. Largest number - comparing numbers for largest figure.

--- bonfire.js
+++ bonfire.js
@@ -1,10 +1,22 @@
 function largestOfFour(arr) {
+  
+  var result = [];
+  
   for (var block=0; block<arr.length; block++){
+    larger_temp = 0;
     for (var i=0; i<arr.length; i++) {
-    if (arr[block][i] > arr[block][i+1]){
-      console.log("It's larger " + arr[i][i]);
+    if (arr[block][i] > larger_temp){
+      larger_temp = arr[block][i];
+      } else {
+      console.log("smaller" + arr[block][i]);
       }
+      result.push(larger_temp);
     }
   }
+  console.log("Result ->" + result);
   return arr;
 }
+
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+
+// console.log(largestOfFour([[1,2,3,4],[5,6,7,8]]));

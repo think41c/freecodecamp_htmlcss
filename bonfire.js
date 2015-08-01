980ac7342f49721eaa519ebd19a8cba9048c2922
Tony DiNitto <think41c@gmail.com>
2015-08-01 17:58:36 -0500

    Waypoint 89. Largest number - initial commit.

--- bonfire.js
+++ bonfire.js
@@ -1,9 +1,6 @@
-function titleCase(str) {
-  x = (str.split(" "));
-  for (var i=0; i<x.length; i++){
-    console.log(x[i].charAt(0).toUpperCase());
-  };
-  return str;
+function largestOfFour(arr) {
+  // You can do this!
+  return arr;
 }
 
-titleCase("I'm a little tea pot");
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);

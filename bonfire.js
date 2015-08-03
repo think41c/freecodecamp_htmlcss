19406eb78048c1e09ac5b06345cc25d422fbf7de
Tony DiNitto <think41c@gmail.com>
2015-08-02 21:38:55 -0500

    Demonstrating JavaScript recursion for exponents.

--- bonfire.js
+++ bonfire.js
@@ -1,18 +1,8 @@
-function largestOfFour(arr) {
-  
-  var result = [];
-  
-  for (var block=0; block<arr.length; block++){
-    larger_temp = 0;
-    for (var i=0; i<arr.length; i++) {
-      if (arr[block][i] > larger_temp) {
-        larger_temp = arr[block][i];
-        } 
-      }
-    result.push(larger_temp);
-  }
-  console.log("Result ->" + result);
-  return arr;
+function power ( base , exponent ) {
+if ( exponent == 0)
+  return 1;
+else
+  return base * power( base , exponent - 1);
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(power (2 , 3));

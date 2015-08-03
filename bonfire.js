22da4add349eaed9357748fa1ea4c74611e3773a
Tony DiNitto <think41c@gmail.com>
2015-08-02 22:38:44 -0500

    Demonstrating JavaScript function to count letters - letters taken as paramaters now.

--- bonfire.js
+++ bonfire.js
@@ -1,10 +1,10 @@
-function countBs(stringy){
+function countBs(stringy, searchChar){
   var counter = 0;
   for (var i=0; i<stringy.length; i++){
     console.log(stringy.charAt(i));
-    if (stringy.charAt(i) == "B")
+    if (stringy.charAt(i) == searchChar)
       counter += 1;
   }
   return counter;
 }
-console.log(countBs("There BBBz"));
+console.log(countBs("There BBBz", "p"));
